4bcdeb05786a1e6953dd106e62ec92e5384e007d
Lucas Bicudo <lucbic@gmail.com>
2018-07-26 23:51:04 -0300

    fist commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="app">
+  <navigation-bar />
+  <content />
+</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+    NavigationBar, Content
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.app {
+  display: flex;
+  align-items: stretch;
+}
+</style>
