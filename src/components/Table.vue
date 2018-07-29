df12468709161f7e1d4c61a9295081be241374d6
Lucas Bicudo <lucbic@gmail.com>
2018-07-29 18:33:31 -0300

    Add Table component

---
+++ src/components/Table.vue
@@ -0,0 +1,58 @@
+<template>
+<div class="table" :class="{ 'table--open': data.open }" @click="toggleTable(index)">
+  <span class="table__number">{{ number }}</span>
+</div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'Table',
+  props: [
+    'index',
+    'data'
+  ],
+  computed: {
+    number () { return this.index + 1 }
+  },
+  methods: {
+    ...mapActions(['toggleTable'])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.table {
+  // shape
+  background: $color__table;
+  width: $size__table;
+  height: $size__table;
+  border-radius: ($size__table / 2);
+
+  // display
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  // transitions
+  transition:
+    border-radius ease-in-out $time__table-transition,
+    background ease-in-out $time__table-transition;
+
+  // modifier
+  &--open {
+    background: $color__table--open;
+    border-radius: 0;
+  }
+
+  /* children */
+  &__number {
+    // font
+    font-size: $fs__table;
+    font-family: $ff__roboto;
+    font-weight: 700;
+    color: $color__table-number;
+  }
+}
+</style>
