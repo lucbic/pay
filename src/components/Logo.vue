3685b898dfe7b92dad010326841a7a63aab4df80
Lucas Bicudo <lucbic@gmail.com>
2018-07-29 22:00:51 -0300

    Add Logo component

---
+++ src/components/Logo.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="logo">
+  <img class="logo__image" src="/static/img/burguer-logo.jpg" alt="logo">
+  <h1 class="logo__title">Hamburgueria</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Logo'
+}
+</script>
+
+<style lang="scss" scoped>
+
+.logo {
+  background: white;
+  padding: 4px 10px;
+  border-radius: $br__default;
+  border: 6px double $color__border-default;
+  width: fit-content;
+
+  &__image {
+    display: block;
+    margin: 0 auto;
+    width: 70px;
+  }
+
+  &__title {
+    text-transform: uppercase;
+    font-family: $ff__roboto;
+    font-size: $fs__logo;
+    color: $color__text-default;
+    margin: 0;
+  }
+}
+</style>
