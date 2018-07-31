e7063016273d20c7b6aa803ae1aff07aa103d16b
Lucas Bicudo <lucbic@gmail.com>
2018-07-31 19:07:33 -0300

    Add getTableOrders and getClientOrders to store getters

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,7 +2,7 @@ export const getCurrentTableIndex = state => state.currentTableIndex
 
 export const getTables = state => state.tables
 
-export const getTable = state => index => state.tables[index]
+export const getTable = state => tableIndex => state.tables[tableIndex]
 
 export const getClients = state => ids => {
   let clients = []
@@ -12,4 +12,51 @@ export const getClients = state => ids => {
   })
 
   return clients
+}
+
+export const getTableOrders = state => tableIndex => {
+  let orders = []
+  const ordersData = state.tables[tableIndex].orders
+
+  ordersData.forEach(orderData => {
+    const product = state.products.find(x => x.id === orderData.product_id)
+    const client = state.clients.find(x => x.id === orderData.client_id)
+
+    let order = {
+      product: product.name,
+      productId: product.id,
+      client: client.name,
+      clientId: client.id,
+      amount: orderData.amount,
+      price: product.price,
+      status: orderData.status
+    }
+
+    orders.push(order)
+  })
+
+  return orders
+}
+
+export const getClientOrders = state => (tableIndex, id) => {
+  let orders = []
+  const ordersData = state.tables[tableIndex].orders
+
+  ordersData.forEach(orderData => {
+    if (orderData.client_id !== id) { return }
+
+    const product = state.products.find(x => x.id === orderData.product_id)
+
+    let order = {
+      product: product.name,
+      productId: product.id,
+      amount: orderData.amount,
+      price: product.price,
+      status: orderData.status
+    }
+
+    orders.push(order)
+  })
+
+  return orders
 }
